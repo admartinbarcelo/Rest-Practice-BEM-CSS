3d9ceb70ad281ba4ab15085585cfd0219beaf20e
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-31 12:03:06 +0200

    first commit for constants folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import './Navbar.css'
 
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+
+import images from '@/constants/images';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo"/>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
