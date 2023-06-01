fcf71b9853b2be311abf96e56c37c40553f8e900
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-01 17:13:41 +0200

    fixed padding

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-unused-vars */
 import "./Header.css";
 
-// import { images } from "../../constants";
 import images from "../../constants/images";
 import { SubHeading } from "../../components";
 
 const Header = () => {
   return (
-    <div className="app__header app__wrapper section__heading" id="home">
+    <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
-        <SubHeading title="Chase the new flavor"/>
+        <SubHeading title="Chase the new flavor" />
         <h1 className="app__header-h1">The Key to fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>hello</p>
+        <button className="custom__button">Explore Menu</button>
       </div>
-        <div className="app__wrapper_img">
 
-        </div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header img" />
+      </div>
     </div>
   );
 };
