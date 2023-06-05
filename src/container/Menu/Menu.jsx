3c8e161728d6541e79acbd373f68a1480307c553
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-05 11:18:57 +0200

    rendering title from menuItem on Menu's map's

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -16,7 +16,7 @@ const Menu = () => {
           <p className="app__menu_heading">Wine & Beer</p>
           <div className="app__menu_items">
             {data.wines.map((wine, index) => (
-              <MenuItem key={wine.title + index} title={wine.price} price={wine.price} tags={wine.tags}/>
+              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
             ))}
           </div>
         </div>
@@ -37,7 +37,7 @@ const Menu = () => {
 
       </div>
         <div style={{marginTop: '15px'}}>
-        <button type="button" className="custom__button"></button>
+        <button type="button" className="custom__button">View More</button>
 
         </div>
 
