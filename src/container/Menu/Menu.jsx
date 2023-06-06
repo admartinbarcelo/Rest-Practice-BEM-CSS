ac3ac185a1b80bf1d32318ac527af127ce1e3fb2
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-06 16:53:01 +0200

    finishing menu component and exporting data

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -12,35 +12,45 @@ const Menu = () => {
       </div>
 
       <div className="app__menu-menu">
-        <div className="app__menu-menu_wine flex__center">
+      <div className="app__menu-menu_wine flex__center">
           <p className="app__menu-menu_heading">Wine & Beer</p>
-          <div className="app__menu_items">
+          <div className="app__menu_menu_items">
+            {/* export data from constants and map title price and tags for cocktails */}
             {data.wines.map((wine, index) => (
-              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
+              <MenuItem
+                key={wine.title + index}
+                title={wine.title}
+                price={wine.price}
+                tags={wine.tags}
+              />
             ))}
           </div>
         </div>
 
         <div className="app__menu-menu_img">
-            <img src={images.menu} alt="menu img" />
+          <img src={images.menu} alt="menu img" />
         </div>
 
         <div className="app__menu-menu_cocktails flex__center">
           <p className="app__menu-menu_heading">Cocktails</p>
           <div className="app__menu_menu_items">
+            {/* export data from constants and map title price and tags for cocktails */}
             {data.cocktails.map((cocktail, index) => (
-                <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
+              <MenuItem
+                key={cocktail.title + index}
+                title={cocktail.title}
+                price={cocktail.price}
+                tags={cocktail.tags}
+              />
             ))}
           </div>
         </div>
-
-
       </div>
-        <div style={{marginTop: '15px'}}>
-        <button type="button" className="custom__button">View More</button>
-
-        </div>
-
+      <div style={{ marginTop: "15px" }}>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
     </div>
   );
 };
