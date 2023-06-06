edf80cdccfc66b6aa4b94061f940e375e2ca6119
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-06 12:25:49 +0200

    color changes

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({ title, price, tags }) => {
     <div className="app__menuitem">
       <div className="app__menuitem-head">
         <div className="app__menuitem-name">
-          <p className="p__cormorant" style={{ color: "#DCCA86" }}>
+          <p className="p__cormorant" style={{ color: "#DCCA87" }}>
             {title}
           </p> 
         </div>
