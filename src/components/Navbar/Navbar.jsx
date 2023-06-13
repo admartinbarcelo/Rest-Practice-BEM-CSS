dc75a8212adf2d123df74d682a6c8e4bd56cf8df
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-13 14:04:20 +0200

    changing branding

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.gericht} alt="app logo" />
+        <img src={images.brekkie} alt="app logo" />
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans">
